1a59b54a7095a4b061a51f8c0a7e5fec2f9ba23c
vontheworst <152864596+vonceee@users.noreply.github.com>
2026-03-29 16:21:42 +0800

    fix: environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://von-pgn-api.onrender.com/api',
+  apiUrl: 'https://von-pgn-be.onrender.com/api',
   lichessToken: '', // Add your Lichess personal access token here (https://lichess.org/account/oauth/token)
   reverbKey: 'von-chess-key',
   reverbHost: 'von-pgn-api.onrender.com',
