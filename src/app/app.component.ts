2373dd52f67c36832ab79bfb427454206d2d69d7
vontheworst <152864596+vonceee@users.noreply.github.com>
2026-04-01 20:25:10 +0800

    fix: stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastComponent } from './shared/components/toast/toast.component';
 import { FeedbackButtonComponent } from './shared/components/feedback-button/feedback-button.component';
-import { SpeedInsights } from '@vercel/speed-insights/angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToastComponent, FeedbackButtonComponent, SpeedInsights],
+  imports: [RouterOutlet, ToastComponent, FeedbackButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.css',
 })
