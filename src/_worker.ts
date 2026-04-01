263108f4a3b46c1073af15d4735ca6bb72007523
vontheworst <152864596+vonceee@users.noreply.github.com>
2026-04-01 21:16:39 +0800

    fix: route issue

--- src/_worker.ts
+++ src/_worker.ts
@@ -1,5 +1,10 @@
+import { AngularAppEngine } from '@angular/ssr';
+
+const angularApp = new AngularAppEngine();
+
 export default {
   async fetch(request: Request): Promise<Response> {
-    return new Response(null, { status: 404 });
+    const response = await angularApp.handle(request);
+    return response ?? new Response('Not Found', { status: 404 });
   },
 };
