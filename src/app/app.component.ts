484671f4e9347e5d1b9dd9d520a7f7ef4c6c00b2
vontheworst <152864596+vonceee@users.noreply.github.com>
2026-04-01 20:19:10 +0800

    add: speed insights

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastComponent } from './shared/components/toast/toast.component';
 import { FeedbackButtonComponent } from './shared/components/feedback-button/feedback-button.component';
+import { SpeedInsights } from '@vercel/speed-insights/angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToastComponent, FeedbackButtonComponent],
+  imports: [RouterOutlet, ToastComponent, FeedbackButtonComponent, SpeedInsights],
   templateUrl: './app.component.html',
   styleUrl: './app.css',
 })
