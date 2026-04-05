41fe4b31499d4304c0b7833d05696948aa862667
vontheworst <152864596+vonceee@users.noreply.github.com>
2026-04-06 02:30:19 +0800

    Update Angular production environment to use correct microservice URL: von-pgn-microservice.onrender.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://von-pgn-be.onrender.com/api',
-  chessMicroserviceUrl: 'https://von-chess-microservice.onrender.com',
+  chessMicroserviceUrl: 'https://von-pgn-microservice.onrender.com',
   lichessToken: '',
   reverbKey: 'von-chess-key',
   reverbHost: 'von-pgn-be.onrender.com',
